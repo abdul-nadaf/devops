129bbb728e263c6bcc6957d6b587aaf0791ed1fc
abdul-nadaf <abdulnadaf376@gmail.com>
2025-10-03 10:16:05 +0530

    hhh

---
+++ swap.py
@@ -0,0 +1,6 @@
+a = int(input("enter first number:"))
+b = int(input("enter second number:"))
+print(f"before swapping:a ={a},b = {b}")
+a,b=b,a
+print(f"after swapping:a={a},b={b}")
+print("please enter valid integers.")
